01d8f5e0a878e978f00fc625a7210e48b093fbca
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-10-28 15:02:10 -0500

    Proyecto funcionando

--- src/app/api/sms/route.js
+++ src/app/api/sms/route.js
@@ -1,5 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import twilio from 'twilio';
 
-export function GET(){
-    return NextResponse.json({message: 'hola mundo'})
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+// const userPhoneNumber = process.env.USER_PHONE_NUMBER;
+
+const client = twilio(accountSid, authToken);
+
+export async function POST(request) {
+  try {
+    const data = await request.json()
+    console.log('data: ', data)
+    const message = await client.messages.create({
+      body: data.message,
+      from: twilioPhoneNumber,
+      to: data.phone,
+    });
+    console.log(message.sid);
+    return NextResponse.json({ message: 'Message Send' });
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      { message: 'Error sending message' },
+      { status: 400 }
+    );
+  }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,59 @@
+"use client"
 import React from 'react'
 
 const Page = () => {
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const sms = {
+      phone: formData.get('phone'),
+      message: formData.get('message')
+    }
+    const resp = await fetch('/api/sms', {
+      method: 'POST',
+      body: JSON.stringify(sms),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await resp.json()
+    console.log('dtaa: ', data)
+    alert('Message sent')
+  }
+
   return (
-    <div>Page</div>
+    <div className='flex items-center justify-center h-screen'>
+      <form
+        className='bg-slate-900 p-10'
+        onSubmit={onSubmit}
+      >
+        <h1 className='text-white text-3xl font-bold'>Send an SMS</h1>
+        <label className='text-white block my-4'>
+          Write your phone number here:
+        </label>
+        <input
+          name='phone'
+          className='px-3 py-1 rounded-md block w-full'
+          type='tel'
+          placeholder='Write your number here'
+          autoComplete='off'
+        />
+        <label className='text-white block my-4'>
+          Write your message here:
+        </label>
+        <textarea
+          className='px-3 py-1 rounded-md block w-full'
+          name='message'
+          placeholder='Write your message here'
+        ></textarea>
+        <button
+          className='bg-blue-500 text-white px-3 py-1 rounded-md block mt-4'
+        >
+          Send
+        </button>
+      </form>
+    </div>
   )
 }
 
